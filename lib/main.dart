4c325f888bf42aa34bbd0d0403a9de03852e21af
Mohamed-lehbib <abeidnamohamedlehbib@gmail.com>
2023-10-16 21:20:31 +0000

    adding a blocBuilder and BlocBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_counter_cubit/cubits/counter/counter_cubit.dart';
+import 'package:my_counter_cubit/other_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,13 +32,43 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          '${BlocProvider.of<CounterCubit>(
-            context,
-            listen: true,
-          ).state.counter}',
-          style: TextStyle(fontSize: 52.0),
+      body: BlocListener<CounterCubit, CounterState>(
+        listener: (context, state) {
+          if (state.counter == 3) {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  content: Text('counter is ${state.counter}'),
+                );
+              },
+            );
+          } else if (state.counter == -1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) {
+                return OtherPage();
+              }),
+            );
+          }
+        },
+        child: BlocBuilder<CounterCubit, CounterState>(
+          builder: (context, state) {
+            return Center(
+              //this works without the BlocBuilder
+              // child: Text(
+              //   '${BlocProvider.of<CounterCubit>(
+              //     context,
+              //     listen: true,
+              //   ).state.counter}',
+              //   style: TextStyle(fontSize: 52.0),
+              // ),
+              child: Text(
+                '${state.counter}',
+                style: TextStyle(fontSize: 52.0),
+              ),
+            );
+          },
         ),
       ),
       floatingActionButton: Row(
